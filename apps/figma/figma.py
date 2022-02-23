5e1b2dfed54d2e5ff3ef839aed08f1b06c96e95c
pauline <github@paulinecat.com>
2022-02-23 10:56:41 -0500

    Update figma commands for windows

--- apps/figma/figma.py
+++ apps/figma/figma.py
@@ -101,3 +101,159 @@ class figma_actions:
 
     def figma_find_next_frame():
         """Fine next frame"""
+
+    def figma_bold():
+        """Bold"""
+
+    def figma_underline():
+        """Underline"""
+
+    def figma_strikethrough():
+        """Strike Through"""
+
+    def figma_transform_list():
+        """Turn into a list"""
+
+    def figma_text_align_left():
+        """Text align left"""
+
+    def figma_text_align_right():
+        """Text align right"""
+
+    def figma_text_align_center():
+        """Text align center"""
+
+    def figma_text_align_justified():
+        """Text align justified"""
+
+    def figma_adjust_font_size_up():
+        """Adjust font size bigger"""
+
+    def figma_adjust_font_size_down():
+        """Adjust font size smaller"""
+
+    def figma_adjust_font_weight_up():
+        """Adjust font weight bigger"""
+
+    def figma_adjust_font_weight_down():
+        """Adjust font weight smaller"""
+
+    def figma_adjust_letter_spacing_up():
+        """Adjust letter spacing bigger"""
+
+    def figma_adjust_letter_spacing_down():
+        """Adjust letter spacing smaller"""
+
+    def figma_adjust_line_height_up():
+        """Adjust line height bigger"""
+
+    def figma_adjust_line_height_down():
+        """Adjust line height smaller"""
+
+    def figma_select_all():
+        """Select all"""
+
+    def figma_select_inverse():
+        """Select inverse"""
+
+    def figma_deep_select():
+        """Deep select"""
+
+    def figma_select_layer_menu():
+        """Select layer menu"""
+
+    def figma_select_children():
+        """Select children"""
+
+    def figma_select_parent():
+        """Select parent"""
+
+    def figma_select_next_sibling():
+        """Select next sibling"""
+
+    def figma_select_prev_sibling():
+        """Select previous sibling"""
+
+    def figma_group_selection():
+        """Group selection"""
+
+    def figma_ungroup_selection():
+        """Ungroup selection"""
+
+    def figma_frame_selection():
+        """Frame selection"""
+
+    def figma_toggle_hide_selection():
+        """Show/Hide selection"""
+
+    def figma_toggle_lock_selection():
+        """Lock/Unlock selection"""
+
+    def figma_duplicate():
+        """Duplicate"""
+
+    def figma_rename_selection():
+        """Rename selection"""
+
+    def figma_flip_horizontal():
+        """Flip horizontal"""
+
+    def figma_flip_vertical():
+        """Flip vertical"""
+
+    def figma_use_as_mask():
+        """Use as mask"""
+
+    def figma_bring_forward():
+        """Bring forward"""
+
+    def figma_send_backward():
+        """Send backward"""
+
+    def figma_bring_to_front():
+        """Bring to front"""
+
+    def figma_send_to_back():
+        """Send to back"""
+
+    def figma_align_left():
+        """Align left"""
+
+    def figma_align_right():
+        """Align right"""
+
+    def figma_align_top():
+        """Align top"""
+
+    def figma_align_bottom():
+        """Align bottom"""
+
+    def figma_align_center_horizontal():
+        """Align center horizontal"""
+
+    def figma_align_center_vertical():
+        """Align center vertical"""
+
+    def figma_distribute_spacing():
+        """Distributes spacing"""
+
+    def figma_tidy_up():
+        """Tidy up"""
+
+    def figma_auto_layout():
+        """Auto layout"""
+
+    def figma_remove_auto_layout():
+        """Remove auto layout"""
+
+    def figma_create_component():
+        """Create component"""
+
+    def figma_detach_instance():
+        """Detach instance"""
+
+    def figma_insert_component():
+        """Insert component"""
+
+    def figma_swap_instance():
+        """Swap instance"""
