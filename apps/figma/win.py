933e20231a16e8ca2200ef0f2256db69e9326dec
pauline <github@paulinecat.com>
2022-02-24 23:44:46 -0500

    Added more Figma commands but for windows

--- apps/figma/win.py
+++ apps/figma/win.py
@@ -1,4 +1,4 @@
-from talon import Context, actions
+from talon import Context, actions, ctrl
 ctx = Context()
 ctx.matches = r"""
 os: windows
@@ -41,3 +41,46 @@ class UserActions:
     def figma_next_page(): actions.key('pagedown')
     def figma_find_previous_frame(): actions.key('home')
     def figma_find_next_frame(): actions.key('end')
+    
+    def figma_bold(): actions.key('ctrl-b')
+    def figma_underline(): actions.key('ctrl-u')
+    def figma_strikethrough(): actions.key('shift-ctrl-x')
+    def figma_transform_list(): actions.key('shift-ctrl-7')
+    def figma_text_align_left(): actions.key('alt-ctrl-l')
+    def figma_text_align_right(): actions.key('alt-ctrl-r')
+    def figma_text_align_center(): actions.key('alt-ctrl-t')
+    def figma_text_align_justified(): actions.key('alt-ctrl-j')
+    def figma_adjust_font_size_up(): actions.key('shift-ctrl->')
+    def figma_adjust_font_size_down(): actions.key('shift-ctrl-<')
+    def figma_adjust_font_weight_up(): actions.key('alt-ctrl-.')
+    def figma_adjust_font_weight_down(): actions.key('alt-ctrl-,')
+    def figma_adjust_letter_spacing_up(): actions.key('alt-.')
+    def figma_adjust_letter_spacing_down(): actions.key('alt-,')
+    def figma_adjust_line_height_up(): actions.key('shift-alt->')
+    def figma_adjust_line_height_down(): actions.key('shift-alt-<')
+
+    def figma_select_all(): actions.key('ctrl-a')
+    def figma_select_inverse(): actions.key('shift-ctrl-a')
+
+    def figma_deep_select():
+        ctrl.key_press('ctrl', down=True)
+        ctrl.mouse_click(button=0, wait=16000)
+        ctrl.key_press('ctrl', up=True)
+
+    def figma_select_layer_menu(): 
+        ctrl.key_press('ctrl', down=True)
+        ctrl.mouse_click(button=1, wait=16000)
+        ctrl.key_press('ctrl', up=True)
+        
+        
+    def figma_select_children(): actions.key('enter')
+    def figma_select_parent(): actions.key('shift-enter')
+    def figma_select_next_sibling(): actions.key('tab')
+    def figma_select_prev_sibling(): actions.key('shift-tab')
+    def figma_group_selection(): actions.key('ctrl-g')
+    def figma_ungroup_selection(): actions.key('shift-ctrl-g')
+    # def figma_frame_selection(): actions.key('')
+    # def figma_toggle_hide_selection(): actions.key('')
+    # def figma_toggle_lock_selection(): actions.key('')
+    # def figma_duplicate(): actions.key('')
+    # def figma_rename_selection(): actions.key('')
